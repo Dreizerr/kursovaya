5469d5893bdba9b2d3f0e72c0c3997c30ead0726
Dreizerr <artem39r@mail.ru>
2024-04-30 17:16:51 +0200

    30.04

--- sendData.php
+++ sendData.php
@@ -5,17 +5,3 @@ $textData = $_POST["textData"];
 $textData = htmlspecialchars($textData);
 mysqli_query($db, "INSERT INTO `comments` (`textdata`) VALUES ('$textData')");
 ?>
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
